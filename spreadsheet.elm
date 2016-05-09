205dbeb241925f2bc5ce89a4aa3dbbbb46a3e365
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2016-05-09 20:53:37 +0200

    :art: format doc

--- spreadsheet.elm
+++ spreadsheet.elm
@@ -190,91 +190,90 @@ getCellVal model i j =
 
 
 {-
-  Разберем обработчик события
+Разберем обработчик события
 
-    on "input" targetValue (\value -> Signal.message address (UpdateCell i j value))
+  on "input" targetValue (\value -> Signal.message address (UpdateCell i j value))
 
-    По событию "input" взять target.value из объекта event
-    и вызвать обработчик типа UpdateCell с координатами ячейки и новым значением (target.value) для нее
+  По событию "input" взять target.value из объекта event
+  и вызвать обработчик типа UpdateCell с координатами ячейки и новым значением (target.value) для нее
 
-    address, i, j - уже известны, это аргументы функции cell, они не изменятся.
-    Упростим запись сгруппировав уже известные переменные под новыми функциями:
+  address, i, j - уже известны, это аргументы функции cell, они не изменятся.
+  Упростим запись сгруппировав уже известные переменные под новыми функциями:
 
-      on "input" targetValue (\value -> messageToAddress (updateCellIJ value))
+    on "input" targetValue (\value -> messageToAddress (updateCellIJ value))
 
-    с использованием оператора обратной композиции это можно записать, как
+  с использованием оператора обратной композиции это можно записать, как
 
-      on "input" targetValue (messageToAddress << updateCellIJ)
+    on "input" targetValue (messageToAddress << updateCellIJ)
 
-    (messageToAddress << updateCellIJ) здесь все так же возвращает функицю,
-    которая ждет на вход CellModel и возвращает Message
+  (messageToAddress << updateCellIJ) здесь все так же возвращает функицю,
+  которая ждет на вход CellModel и возвращает Message
 
-    Если мы вернемся к полной записи функций, то это будет выглядеть вот так:
+  Если мы вернемся к полной записи функций, то это будет выглядеть вот так:
 
-      on "input" targetValue (Signal.message address << UpdateCell i j)
+    on "input" targetValue (Signal.message address << UpdateCell i j)
 
-  Разберем, как это работает
+Разберем, как это работает
 
-  Для начала посмотрим на сигнатуру конструктора обработчика событий
+Для начала посмотрим на сигнатуру конструктора обработчика событий
 
-    on : String -> Json.Decode.Decoder a -> a -> Signal.Message
+  on : String -> Json.Decode.Decoder a -> a -> Signal.Message
 
-    on "input" targetValue sendMessage -- совсем, как в Javascript
+  on "input" targetValue sendMessage -- совсем, как в Javascript
 
-  Первый аргумент типа String
-      - имя события, в нашем случае это "input"
-  Второй аргумент типа Json.Decode.Decoder a
-      - функция, которая достанет нужные нам данные из объекта события
-      - в нашем случае это targetValue, вернет поле value из объекта, на котором произойтет событие
-  Третий аргумент sendMessage : a -> Signal.Message - функция, которая должна вернуть Signal.Message
+Первый аргумент типа String
+    - имя события, в нашем случае это "input"
+Второй аргумент типа Json.Decode.Decoder a
+    - функция, которая достанет нужные нам данные из объекта события
+    - в нашем случае это targetValue, вернет поле value из объекта, на котором произойтет событие
+Третий аргумент sendMessage : a -> Signal.Message - функция, которая должна вернуть Signal.Message
 
-  У нас есть все кроме функции sendMessage
+У нас есть все кроме функции sendMessage
 
-  Чтобы ее получить нам нужно использовать 2 функции:
-    1. Signal.message : Address a -> a -> Message
-        - это полиморфная функция из стандартной библиотеки, в нашем случае переменная 'a' имеет тип Action
-       Signal.message : Address Action -> Action -> Message
-    2. UpdateCell : Int -> Int -> CellModel -> Action
-        - наша собственная функция, которая возвращает переменную типа Action, который мы определили выше
+Чтобы ее получить нам нужно использовать 2 функции:
+  1. Signal.message : Address a -> a -> Message
+      - это полиморфная функция из стандартной библиотеки, в нашем случае переменная 'a' имеет тип Action
+     Signal.message : Address Action -> Action -> Message
+  2. UpdateCell : Int -> Int -> CellModel -> Action
+      - наша собственная функция, которая возвращает переменную типа Action, который мы определили выше
 
-  По-русски:
-    1. Signal.message - функция, принимает Address, Action и возвращает Message
-    2. UpdateCell - функция, принимает i, j, value и возвращает тип Action
+По-русски:
+  1. Signal.message - функция, принимает Address, Action и возвращает Message
+  2. UpdateCell - функция, принимает i, j, value и возвращает тип Action
 
-  Как мы видим, вторая функция возвращает тип Action,
-  а первая ожидает переменную типа Action в качестве аргумента,
-  так же вы видим, что нам известно все кроме value типа CellModel
+Как мы видим, вторая функция возвращает тип Action,
+а первая ожидает переменную типа Action в качестве аргумента,
+так же вы видим, что нам известно все кроме value типа CellModel
 
-  Попробуем скомпоновать две эти функции так, чтобы оставить снаружи только CellModel
+Попробуем скомпоновать две эти функции так, чтобы оставить снаружи только CellModel
 
-  Наша цель получить функцию с сигнатурой
-    sendMessage : CellModel -> Message
+Наша цель получить функцию с сигнатурой
+  sendMessage : CellModel -> Message
 
-  Частично применяем 1. функцию, чтобы замкнуть address:
+Частично применяем 1. функцию, чтобы замкнуть address:
 
-    messageToAddress : Action -> Message
-    messageToAddress = Signal.message address
+  messageToAddress : Action -> Message
+  messageToAddress = Signal.message address
 
-  Частично применям 2. функцию, чтобы замнкуть уже известные i и j
+Частично применям 2. функцию, чтобы замнкуть уже известные i и j
 
-    updateCellIJ : CellModel -> Action
-    updateCellIJ = UpdateCell i j
+  updateCellIJ : CellModel -> Action
+  updateCellIJ = UpdateCell i j
 
-  И того у нас получилось 2 новых функции messageToAddress и updateCellIJ, скомпонуем их:
+И того у нас получилось 2 новых функции messageToAddress и updateCellIJ, скомпонуем их:
 
-    sendMessage : CellModel -> Message
-    sendMessage value = messageToAddress << updateCellIJ value
+  sendMessage : CellModel -> Message
+  sendMessage value = messageToAddress << updateCellIJ value
 
-  Такой формат записи равнозначен
+Такой формат записи равнозначен
 
-    sendMessage(x) = messageToAddress(updateCellIJ(x))
+  sendMessage(x) = messageToAddress(updateCellIJ(x))
 
-  Обратная композиция << наших функций в развернутом виде
+Обратная композиция << наших функций в развернутом виде
 
-    (Address Action -> Action -> Message) ->
-      (Int -> Int -> CellModel -> Action) ->
-      (Int -> Int -> CellModel -> Address Action -> Message)
-
+  (Address Action -> Action -> Message) ->
+    (Int -> Int -> CellModel -> Action) ->
+    (Int -> Int -> CellModel -> Address Action -> Message)
 -}
 
 
